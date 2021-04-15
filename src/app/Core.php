96c210ca33e82332a3c6c5f07759a303dc212e97
Prophe1 <sidas.snieska@gmail.com>
2021-04-15 11:29:26 +0300

    Update structure

--- src/app/Core.php
+++ src/app/Core.php
@@ -4,5 +4,27 @@ namespace Prophe1\WPProjectsCore;
 
 final class Core
 {
+    private static $instance = null;
 
+    private function __construct()
+    {
+        $this->setDefinitions();
+    }
+
+    private function setDefinitions()
+    {
+        define('PROPHE1_WP_PROJECT_CORE_DIR', plugin_dir_path(__FILE__) . '../');
+    }
+
+    public static function init(): self
+    {
+        if (self::$instance == null)
+        {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
 }
+
+Core::init();
