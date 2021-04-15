e0af859e8ba560f62beac1a3c2c8a624d35b4761
Prophe1 <sidas.snieska@gmail.com>
2021-04-15 13:15:49 +0300

    Added Image with inners

---
+++ src/app/Blocks/ImageWithInners.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Prophe1\WPProjectsCore\Blocks;
+
+use Prophe1\ACFBlockz\AbstractACFBladeBlock;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+class ImageWithInners extends AbstractACFBladeBlock
+{
+    /**
+     * Hero constructor.
+     * @throws \ReflectionException
+     */
+    public function __construct()
+    {
+        parent::__construct([
+            'title' => __('Image with Inners', 'sage'),
+            'description' => __('Create a Image with Inners block', 'sage'),
+            'category' => 'layout',
+            'icon' => 'businessman',
+            'mode' => 'preview',
+            'dir' => 'WPPRCORE::',
+            'parent' => ['acf/image-with-inners', 'acf/inner-blocks'],
+            'supports' => [
+                'align' => false,
+                'mode' => false,
+                'jsx' => true
+            ],
+        ]);
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function getInnerBlocks()
+    {
+        return [
+            'allowed_blocks' => apply_filters('wp-projects-core_image_with_inners_allowed_blocks', [
+                'core/heading',
+                'core/paragraph',
+                'acf/icon-with-content',
+                'acf/card'
+            ])
+        ];
+    }
+
+    /**
+     * Generate block fields
+     *
+     * @return array
+     * @throws \StoutLogic\AcfBuilder\FieldNameCollisionException
+     */
+    protected function registerFields(): array
+    {
+        $block = new FieldsBuilder($this->name);
+
+        $block
+            ->addImage('image', [
+                'return_format' => 'id'
+            ])
+            ->addSelect('image_position')
+                ->addChoices([
+                    'left' => 'Left',
+                    'right' => 'Right'
+                ]);
+
+        $block
+            ->addSelect('image_alignment')
+                ->addChoices([
+                    'top' => 'Top',
+                    'center' => 'Center'
+                ]);
+
+        $block
+            ->addTrueFalse('shadows', [
+                'label' => 'Enable Shadows'
+            ])
+            ->addTrueFalse('image_fixed', [
+                'label' => 'Fix Image',
+                'conditional_logic' => [
+                    'field' => 'image_alignment',
+                    'operator' => '==',
+                    'value' => 'top',
+                ]
+            ]);
+
+        $block->setLocation('block', '==', sprintf('acf/%s', $this->name));
+
+        return $block->build();
+    }
+}
---
+++ src/resources/views/blocks/image-with-inners.blade.php
@@ -0,0 +1,23 @@
+@php /** @var \Prophe1\WPProjectsCore\Blocks\ImageWithInners $controller */ @endphp
+
+@php
+  $image_alignment = isset($image_alignment) ? $image_alignment : null;
+  $image_fixed = isset($image_fixed) ? $image_fixed : null;
+@endphp
+
+@component('blocks.partials.block-wrap')
+  <div class="container {{ $image_fixed ? 'has-draggable' : '' }}" {{ $image_fixed ? 'data-smTrigger=.desc' : '' }}>
+    <div class="laptop:flex {{ $image_fixed ? '' : ($image_alignment === 'center' ? 'items-center' : '') }} {{ $image_position === 'right' ? 'flex-row-reverse' : '' }}">
+      <div class="flex-1 is-sm-container">
+        <div class="to-drag">
+          {!! \Prophe1\Wp\Image\Render::output($image, 'fr-hero-tablet', [
+          ], ['class' => 'mx-auto mb-4 laptop:mb-0 ' . ($shadows ? 'shadow-4xl' : '')]) !!}
+        </div>
+      </div>
+
+      <div class="desc flex-1 {{ $image_position === 'right' ? 'laptop:mr-7' : 'laptop:ml-7' }}">
+        @include('blocks.partials.block-inners', $controller->getInnerBlocks())
+      </div>
+    </div>
+  </div>
+@endcomponent
