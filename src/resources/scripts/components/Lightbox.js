dd279a2b9a492d9871ddb4bd69d78da834d2ac1f
Mantas Tautvaiša <mantas.tautvaisa@gmail.com>
2021-07-26 11:22:21 +0300

    baguetefix

--- src/resources/scripts/components/Lightbox.js
+++ src/resources/scripts/components/Lightbox.js
@@ -3,7 +3,9 @@ import baguetteBox from 'baguettebox.js';
 export const Lightbox = () => {
 
   const init = () => {
-    baguetteBox.run('.wp-block-gallery, .wp-block-image');
+    baguetteBox.run('.wp-block-gallery, .wp-block-image', {
+      bodyClass: 'overflow-hidden'
+    });
   };
 
   return { init };
